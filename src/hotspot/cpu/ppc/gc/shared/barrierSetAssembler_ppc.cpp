xmas92/jdk

UPSTREAM: JNI Global Handle Non Oracle Platforms

src/hotspot/cpu/ppc/gc/shared/barrierSetAssembler_ppc.cpp
@@ -116,22 +116,34 @@ void BarrierSetAssembler::load_at(MacroAssembler* masm, DecoratorSet decorators,
 void BarrierSetAssembler::resolve_jobject(MacroAssembler* masm, Register value,
                                           Register tmp1, Register tmp2,
                                           MacroAssembler::PreservationLevel preservation_level) {
-  Label done, not_weak, verify;
+  Label done, verify, tagged, weak_tagged;
   __ cmpdi(CCR0, value, 0);
   __ beq(CCR0, done);         // Use NULL as-is.
 
-  __ andi_(tmp1, value, JNIHandles::weak_tag_mask);
-  __ beq(CCR0, not_weak);     // Test for jweak tag.
+  __ andi_(tmp1, value, JNIHandles::tag_mask);
+  __ bne(CCR0, tagged);     // Test for tag.
 
-  // Resolve (untagged) jobject.
-  __ clrrdi(value, value, JNIHandles::weak_tag_size);
-  load_at(masm, IN_NATIVE | ON_PHANTOM_OOP_REF, T_OBJECT,
+  // Resolve local handle
+  load_at(masm, IN_NATIVE | AS_RAW, T_OBJECT,
           value, (intptr_t)0, value, tmp1, tmp2, preservation_level);
   __ b(verify);
 
-  __ bind(not_weak);
+
+  __ bind(tagged);
+  __ andi_(tmp1, value, JNIHandles::weak_tag_mask);
+  __ bne(CCR0, weak_tagged);      // Test for weak tag.
+
+  // Resolve global handle
+  __ clrrdi(value, value, JNIHandles::tag_size);
   load_at(masm, IN_NATIVE, T_OBJECT,
           value, (intptr_t)0, value, tmp1, tmp2, preservation_level);
+
+  // Resolve jweak.
+  __ bind(weak_tagged);
+  __ clrrdi(value, value, JNIHandles::tag_size);
+  load_at(masm, IN_NATIVE | ON_PHANTOM_OOP_REF, T_OBJECT,
+          value, (intptr_t)0, value, tmp1, tmp2, preservation_level);
+
 
   __ bind(verify);
   __ verify_oop(value, FILE_AND_LINE);
@@ -140,7 +152,7 @@ void BarrierSetAssembler::resolve_jobject(MacroAssembler* masm, Register value,
 
 void BarrierSetAssembler::try_resolve_jobject_in_native(MacroAssembler* masm, Register dst, Register jni_env,
                                                         Register obj, Register tmp, Label& slowpath) {
-  __ clrrdi(dst, obj, JNIHandles::weak_tag_size);
+  __ clrrdi(dst, obj, JNIHandles::tag_size);
   __ ld(dst, 0, dst);         // Resolve (untagged) jobject.
 }
 

src/hotspot/cpu/riscv/macroAssembler_riscv.cpp
@@ -512,23 +512,36 @@ void MacroAssembler::debug64(char* msg, int64_t pc, int64_t regs[])
 }
 
 void MacroAssembler::resolve_jobject(Register value, Register thread, Register tmp) {
-  Label done, not_weak;
-  beqz(value, done);           // Use NULL as-is.
+  assert_different_registers(value, tmp, thread);
+  Label done, tagged, weak_tagged;
 
+  beqz(value, done);           // Use NULL as-is.
+  // Test for tag.
+  andi(t0, value, JNIHandles::tag_mask);
+  bnez(t0, tagged);
+
+  // Resolve global handle
+  access_load_at(T_OBJECT, IN_NATIVE | AS_RAW, value, Address(value, 0), tmp, thread);
+  //verify_oop(value);
+  b(done);
+
+  bind(tagged);
   // Test for jweak tag.
   andi(t0, value, JNIHandles::weak_tag_mask);
-  beqz(t0, not_weak);
+  bnez(t0, weak_tagged);
 
+  // Resolve global handle
+  access_load_at(T_OBJECT, IN_NATIVE, value,
+                 Address(value, -JNIHandles::global_tag_value), tmp, thread);
+  b(done);
+
+  bind(weak_tagged);
   // Resolve jweak.
   access_load_at(T_OBJECT, IN_NATIVE | ON_PHANTOM_OOP_REF, value,
                  Address(value, -JNIHandles::weak_tag_value), tmp, thread);
   verify_oop(value);
-  j(done);
+  // b(done);
 
-  bind(not_weak);
-  // Resolve (untagged) jobject.
-  access_load_at(T_OBJECT, IN_NATIVE, value, Address(value, 0), tmp, thread);
-  verify_oop(value);
   bind(done);
 }
 

src/hotspot/cpu/riscv/stubGenerator_riscv.cpp
@@ -3806,12 +3806,7 @@ class StubGenerator: public StubCodeGenerator {
 
   static void jfr_epilogue(MacroAssembler* _masm, Register thread) {
     __ reset_last_Java_frame(true);
-    Label null_jobject;
-    __ beqz(x10, null_jobject);
-    DecoratorSet decorators = ACCESS_READ | IN_NATIVE;
-    BarrierSetAssembler* bs = BarrierSet::barrier_set()->barrier_set_assembler();
-    bs->load_at(_masm, decorators, T_OBJECT, x10, Address(x10, 0), c_rarg0, thread);
-    __ bind(null_jobject);
+    __ resolve_jobject(x10, c_rarg0, thread);
   }
   // For c2: c_rarg0 is junk, call to runtime to write a checkpoint.
   // It returns a jobject handle to the event writer.
@@ -3838,6 +3833,7 @@ class StubGenerator: public StubCodeGenerator {
     address the_pc = __ pc();
     jfr_prologue(the_pc, _masm, xthread);
     __ call_VM_leaf(CAST_FROM_FN_PTR(address, JfrIntrinsicSupport::write_checkpoint), 1);
+
     jfr_epilogue(_masm, xthread);
     __ leave();
     __ ret();

xmas92/zgc

Co-authored-by: TheRealMDoerr <martin.doerr@sap.com>
Co-authored-by: zhaixiang <zhaixiang@loongson.cn>

src/hotspot/cpu/ppc/gc/shared/barrierSetAssembler_ppc.cpp
@@ -116,22 +116,29 @@ void BarrierSetAssembler::load_at(MacroAssembler* masm, DecoratorSet decorators,
 void BarrierSetAssembler::resolve_jobject(MacroAssembler* masm, Register value,
                                           Register tmp1, Register tmp2,
                                           MacroAssembler::PreservationLevel preservation_level) {
-  Label done, not_weak, verify;
+  Label done, tagged, weak_tagged, verify;
   __ cmpdi(CCR0, value, 0);
   __ beq(CCR0, done);         // Use NULL as-is.
 
-  __ andi_(tmp1, value, JNIHandles::weak_tag_mask);
-  __ beq(CCR0, not_weak);     // Test for jweak tag.
+  __ andi_(tmp1, value, JNIHandles::tag_mask);
+  __ bne(CCR0, tagged);       // Test for tag.
 
-  // Resolve (untagged) jobject.
-  __ clrrdi(value, value, JNIHandles::weak_tag_size);
-  load_at(masm, IN_NATIVE | ON_PHANTOM_OOP_REF, T_OBJECT,
-          value, (intptr_t)0, value, tmp1, tmp2, preservation_level);
+  __ access_load_at(T_OBJECT, IN_NATIVE | AS_RAW, // no uncoloring
+                    value, (intptr_t)0, value, tmp1, tmp2, preservation_level);
   __ b(verify);
 
-  __ bind(not_weak);
-  load_at(masm, IN_NATIVE, T_OBJECT,
-          value, (intptr_t)0, value, tmp1, tmp2, preservation_level);
+  __ bind(tagged);
+  __ andi_(tmp1, value, JNIHandles::weak_tag_mask);
+  __ clrrdi(value, value, JNIHandles::tag_size); // Untag.
+  __ bne(CCR0, weak_tagged);   // Test for jweak tag.
+
+  __ access_load_at(T_OBJECT, IN_NATIVE,
+                    value, (intptr_t)0, value, tmp1, tmp2, preservation_level);
+  __ b(verify);
+
+  __ bind(weak_tagged);
+  __ access_load_at(T_OBJECT, IN_NATIVE | ON_PHANTOM_OOP_REF,
+                    value, (intptr_t)0, value, tmp1, tmp2, preservation_level);
 
   __ bind(verify);
   __ verify_oop(value, FILE_AND_LINE);
@@ -140,7 +147,7 @@ void BarrierSetAssembler::resolve_jobject(MacroAssembler* masm, Register value,
 
 void BarrierSetAssembler::try_resolve_jobject_in_native(MacroAssembler* masm, Register dst, Register jni_env,
                                                         Register obj, Register tmp, Label& slowpath) {
-  __ clrrdi(dst, obj, JNIHandles::weak_tag_size);
+  __ clrrdi(dst, obj, JNIHandles::tag_size);
   __ ld(dst, 0, dst);         // Resolve (untagged) jobject.
 }
 

src/hotspot/cpu/riscv/macroAssembler_riscv.cpp
@@ -511,23 +511,35 @@ void MacroAssembler::debug64(char* msg, int64_t pc, int64_t regs[])
 }
 
 void MacroAssembler::resolve_jobject(Register value, Register tmp1, Register tmp2) {
-  Label done, not_weak;
-  beqz(value, done);           // Use NULL as-is.
+  assert_different_registers(value, tmp1, tmp2);
+  Label done, tagged, weak_tagged;
 
+  beqz(value, done);           // Use NULL as-is.
+  // Test for tag.
+  andi(t0, value, JNIHandles::tag_mask);
+  bnez(t0, tagged);
+
+  // Resolve global handle
+  access_load_at(T_OBJECT, IN_NATIVE | AS_RAW, value, Address(value, 0), tmp1, tmp2);
+  verify_oop(value);
+  j(done);
+
+  bind(tagged);
   // Test for jweak tag.
   andi(t0, value, JNIHandles::weak_tag_mask);
-  beqz(t0, not_weak);
+  bnez(t0, weak_tagged);
 
+  // Resolve global handle
+  access_load_at(T_OBJECT, IN_NATIVE, value,
+                 Address(value, -JNIHandles::global_tag_value), tmp1, tmp2);
+  j(done);
+
+  bind(weak_tagged);
   // Resolve jweak.
   access_load_at(T_OBJECT, IN_NATIVE | ON_PHANTOM_OOP_REF, value,
                  Address(value, -JNIHandles::weak_tag_value), tmp1, tmp2);
   verify_oop(value);
-  j(done);
 
-  bind(not_weak);
-  // Resolve (untagged) jobject.
-  access_load_at(T_OBJECT, IN_NATIVE, value, Address(value, 0), tmp1, tmp2);
-  verify_oop(value);
   bind(done);
 }
 

src/hotspot/cpu/riscv/stubGenerator_riscv.cpp
@@ -3802,14 +3802,8 @@ class StubGenerator: public StubCodeGenerator {
 
   static void jfr_epilogue(MacroAssembler* _masm) {
     __ reset_last_Java_frame(true);
-    Label null_jobject;
-    __ beqz(x10, null_jobject);
-    DecoratorSet decorators = ACCESS_READ | IN_NATIVE;
-    BarrierSetAssembler* bs = BarrierSet::barrier_set()->barrier_set_assembler();
-    bs->load_at(_masm, decorators, T_OBJECT, x10, Address(x10, 0), t0, t1);
-    __ bind(null_jobject);
+    __ resolve_jobject(x10, t0, t1);
   }
-
   // For c2: c_rarg0 is junk, call to runtime to write a checkpoint.
   // It returns a jobject handle to the event writer.
   // The handle is dereferenced and the return value is the event writer oop.
@@ -3835,6 +3829,7 @@ class StubGenerator: public StubCodeGenerator {
     address the_pc = __ pc();
     jfr_prologue(the_pc, _masm, xthread);
     __ call_VM_leaf(CAST_FROM_FN_PTR(address, JfrIntrinsicSupport::write_checkpoint), 1);
+
     jfr_epilogue(_masm);
     __ leave();
     __ ret();